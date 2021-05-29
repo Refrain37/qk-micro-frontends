3ed81b51d57fd9701023186ed4937a9449ffbf62
许泓杰 <965926581@qq.com>
2021-05-29 23:00:12 +0800

    05/29 child1 improve markdown-editor component

--- child1/src/components/MarkdowmEditor.vue
+++ child1/src/components/MarkdowmEditor.vue
@@ -1,38 +1,30 @@
 <template>
   <v-md-editor
-    v-model="content"
     :disabled-menus="[]"
     @save="handleSave"
     @upload-image="handleUploadImage"
-    height="700px"
   ></v-md-editor>
 </template>
 
 <script lang="ts">
-  import { defineComponent, ref } from 'vue'
+  import { defineComponent } from 'vue'
 
   export default defineComponent({
-    setup() {
-      const content = ref<string>('')
+    emits: ['uploadImg', 'save'],
+    setup(props, context) {
+      // 上传图片
       const handleUploadImage = (
         event: any,
         insertImage: any,
         files: any[]
       ) => {
-        console.log(files)
-        insertImage({
-          url:
-            'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1269952892,3525182336&fm=26&gp=0.jpg',
-          desc: '七龙珠',
-          width: '300',
-          height: 'auto',
-        })
+        context.emit('uploadImg', event, insertImage, files)
       }
+      // 保存
       const handleSave = (text: any, html: any) => {
-        console.log(text, html)
+        context.emit('save', text, html)
       }
       return {
-        content,
         handleSave,
         handleUploadImage,
       }
--- child1/src/views/articleEditor/index.vue
+++ child1/src/views/articleEditor/index.vue
@@ -1,15 +1,52 @@
 <template>
-  <div class="article-editor-container"></div>
+  <div class="article-editor-container">
+    <h1>markdown-editor</h1>
+    <markdown-editor
+      class="markdown-editor"
+      v-model="content"
+      @uploadImg="handleUploadImage"
+      @save="handleSave"
+    ></markdown-editor>
+  </div>
 </template>
 
 <script lang="ts">
-  import { defineComponent } from 'vue'
+  import { defineComponent, ref } from 'vue'
+  import MarkdownEditor from '../../components/MarkdowmEditor.vue'
 
   export default defineComponent({
+    components: {
+      MarkdownEditor,
+    },
     setup() {
-      return {}
+      const { content, handleUploadImage, handleSave } = useMarkDownEditor()
+      return {
+        content,
+        handleUploadImage,
+        handleSave,
+      }
     },
   })
+  // markdown-editor
+  function useMarkDownEditor() {
+    const content = ref<string>('')
+    const handleUploadImage = (event: any, insertImage: any, files: any[]) => {
+      console.log(files, 'parent')
+    }
+    const handleSave = (text: any, html: any) => {
+      console.log(text)
+    }
+
+    return {
+      content,
+      handleUploadImage,
+      handleSave,
+    }
+  }
 </script>
 
-<style scoped></style>
+<style scoped>
+  .markdown-editor {
+    height: 500px;
+  }
+</style>
